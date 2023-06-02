56174fa9809dbad6f91cc6cab3aa5d2e2ce06f8b
queenGambitChessGuy <133424742+queenGambitChessGuy@users.noreply.github.com>
2023-06-02 21:37:11 +0300

    Add files via upload

---
+++ readENVCORNmore.js
@@ -0,0 +1,15 @@
+function back(){
+    window.location = "index.html"
+}
+
+function next(){
+    window.location = "readENVCORNmore2.html"
+}
+
+function prev(){
+    window.location = "readENVCORNmore1.html"
+}
+
+function minigame(){
+    window.location = "minigame.html"
+}
